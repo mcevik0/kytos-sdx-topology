361ace8cdcfbfe41a323ad4e057e3460cbb488a5
Debian <admin@ip-172-31-13-114.ec2.internal>
2022-02-04 14:42:22 +0000

    test nodes and links in sdx

--- tests/functional/test_sdx_topology_api.py
+++ tests/functional/test_sdx_topology_api.py
@@ -8,6 +8,14 @@ import requests
 KYTOS_TOPOLOGY_URL = "http://localhost:8181/api/kytos/topology/v3/"
 
 
+def test_validate(valid_data):
+    """ test a GET request to SDX validate"""
+    response = requests.post(
+            url=valid_data["url"], data=json.dumps(valid_data["payload"]),
+            headers=valid_data["headers"])
+    assert response.status_code == 200
+
+
 def test_kytos_topology(api_data):
     """ test a GET request to SDX validate end_to_end_test_1_1"""
     response = requests.get(
@@ -53,8 +61,8 @@ def test_sdx_topology_id(api_data):
     assert response.json()["id"].split(":")[3] == "amlight.net"
 
 
-def test_sdx_topology_link(api_data):
-    """ test the SDX topology link get request end_to_end_test_1_5 """
+def test_kytos_topology_link(api_data):
+    """ test the kytos topology link get request end_to_end_test_1_5 """
     response = requests.get(
             url=KYTOS_TOPOLOGY_URL, headers=api_data["headers"])
     assert response.status_code == 200
@@ -66,9 +74,21 @@ def test_sdx_topology_link(api_data):
     assert response.status_code == 201
 
 
-def test_validate(valid_data):
-    """ test a GET request to SDX validate"""
-    response = requests.post(
-            url=valid_data["url"], data=json.dumps(valid_data["payload"]),
-            headers=valid_data["headers"])
+def test_sdx_topology_nodes(api_data):
+    """ test the SDX topology nodes get request end_to_end_test_1_6 """
+    response = requests.get(
+            url=api_data["url"]+"topology", headers=api_data["headers"])
     assert response.status_code == 200
+    assert "nodes" in response.json()
+    assert isinstance(response.json()["nodes"], list)
+    assert len(response.json()["nodes"]) != 0
+
+
+def test_sdx_topology_links(api_data):
+    """ test the SDX topology links get request end_to_end_test_1_7 """
+    response = requests.get(
+            url=api_data["url"]+"topology", headers=api_data["headers"])
+    assert response.status_code == 200
+    assert "links" in response.json()
+    assert isinstance(response.json()["links"], list)
+    assert len(response.json()["links"]) != 0
