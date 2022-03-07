59bd0d640c471da83d5348b45dbd0dfa78d17d42
Debian <admin@ip-172-31-2-93.ec2.internal>
2022-03-07 04:10:40 +0000

    request.json

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,18 +5,6 @@ import os
 import json
 import pytest
 from kytos.core.events import KytosEvent
-from .run import create_app
-
-
-@pytest.fixture(scope="session")
-def flask_app():
-    ''' flask app '''
-    abs_file_path = os.path.abspath(os.path.dirname(__file__))
-    openapi_path = os.path.join(abs_file_path, "../", "app")
-    os.environ["SPEC_PATH"] = openapi_path
-
-    app = create_app()
-    return app
 
 
 @pytest.fixture
@@ -24,6 +12,17 @@ def event():
     '''Returns a KytosEvents instance'''
     kevent = KytosEvent()
     return kevent
+
+
+@pytest.fixture
+def valid_request():
+    """ Build json_data topology """
+    actual_dir = os.getcwd()
+    topology_params = actual_dir + "/tests/request.json"
+    with open(topology_params, encoding="utf8") as json_file:
+        data = json.load(json_file)
+        json_file.close()
+    return data
 
 
 @pytest.fixture
--- tests/unit/test_util.py
+++ tests/unit/test_util.py
@@ -1,8 +1,10 @@
 """
 SDX Topology util Unit test
 """
+from flask import Request
 import numpy as np
 import pandas as pd
+from openapi_core.validation.request.validators import RequestValidator
 from napps.kytos.sdx_topology import utils  # pylint: disable=E0401
 
 pd.set_option('display.max_rows', 500)
@@ -46,3 +48,33 @@ def test_load_spec():
     '''test load_spec'''
     content = utils.load_spec().content()
     assert content['servers'][0]['url'] == '/api/kytos/sdx_topology'
+
+
+def test_validate_request(valid_data):
+    ''' test_validate_request '''
+    spec = utils.load_spec()
+    request = Request.from_values(
+            '/api/kytos/sdx_topology/v1/validate',
+            method='POST')
+    request.full_url_pattern = '/api/kytos/sdx_topology/v1/validate'
+    print(dir(request))
+    print(request)
+    validator = RequestValidator(spec)
+    result = validator.validate(request)
+    error_response = {"errors": "no errors"}
+    if result.errors:
+        errors = result.errors[0]
+        if hasattr(errors, "schema_errors"):
+            schema_errors = errors.schema_errors[0]
+            error_response = {
+                "error_message": schema_errors.message,
+                "error_validator": schema_errors.validator,
+                "error_validator_value": schema_errors.validator_value,
+                "error_path": list(schema_errors.path),
+                "error_schema": schema_errors.schema,
+                "error_schema_path": list(schema_errors.schema_path),
+                }
+        else:
+            error_response = {"errors": errors}
+    print(error_response)
+    assert error_response["errors"] == "no errors"
