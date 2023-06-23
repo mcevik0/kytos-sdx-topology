2310e75b1281b4aa0e1866badb8574c42ff36f78
Ubuntu <ubuntu@ip-172-31-3-123.ec2.internal>
2023-06-23 22:13:53 +0000

    update main

--- app/main.py
+++ app/main.py
@@ -26,6 +26,7 @@ class Main(KytosNApp):  # pylint: disable=R0904
 
         So, if you have any setup routine, insert it here.
         """
+        self.event_info = {} # pylint: disable=W0201
 
     def execute(self):
         """Run after the setup method execution.
@@ -42,59 +43,24 @@ class Main(KytosNApp):  # pylint: disable=R0904
         If you have some cleanup procedure, insert it here.
         """
 
-    @staticmethod
-    def get_kytos_topology():
-        """retrieve topology from API"""
-        kytos_topology = requests.get(settings.KYTOS_TOPOLOGY_URL).json()
-        log.info("######### get_kytos_topology #########")
-        # log.info(kytos_topology)
-        return kytos_topology["topology"]
-
 
     @listen_to("kytos/topology.*")
-    def load_topology(self, event=None):  # pylint: disable=W0613
+    def listen_topology(self, event=None):  # pylint: disable=W0613
         """ Function meant for listen topology """
-        log.info("######### load_topology #########")
-        event_type = 0
-        admin_events = [
-                "kytos/topology.switch.enabled",
-                "kytos/topology.switch.disabled"]
-        operational_events = [
-                "kytos/topology.link_up",
-                "kytos/topology.link_down"]
-        if event.name in admin_events:
-            event_type = 1
-        elif event.name in operational_events and event.timestamp is not None:
-            event_type = 2
+        log.info("######### listen topology #########")
+        self.event_info = {} # pylint: disable=W0201
         try:
-            topology = self.get_kytos_topology()
-            topology_info = {
-                    "event": event,
-                    "event_type": event_type,
-                    "event_name": event.name,
-                    "timestamp": event.timestamp,
-                    "topology": topology}
-            # log.info(topology_info["event"])
-            log.info("######### Topology_info event_type #########")
-            log.info(topology_info["event_type"])
-            log.info("######### Topology_info event_name #########")
-            log.info(topology_info["event_name"])
-            log.info("######### Topology_info timestamp #########")
-            log.info(topology_info["timestamp"])
-            # log.info(topology_info[topology])
-            if event_type != 0:
-                pass
+            topology = requests.get(settings.KYTOS_TOPOLOGY_URL).json()
+            self.event_info = {"event": event, "topology": topology} # pylint: disable=W0201
+            requests.post(settings.SDX_LC).json(self.event_info)
         except Exception as err:  # pylint: disable=W0703
-            log.info("######### load_topology error #########")
+            log.info("######### listen topology error #########")
             log.info(err)
+        log.info(self.event_info)
+        return self.event_info
 
-    @rest("v2/get_sdx_topology", methods=["GET"])
-    def get_sdx_topology(self):
-        """ consume the SDX Topology """
-        log.info("######### get_sdx_topology #########")
-        sdx_topology = self.load_topology()
-        log.info("######### sdx_topology #########")
-        log.info(sdx_topology)
-        if sdx_topology:
-            return sdx_topology
-        return {}
+    @rest("v2/listen_topology", methods=["GET"])
+    def get_listen_topology(self):
+        """ consume call listen Topology """
+        log.info("######### get_listen_topology #########")
+        return self.listen_topology()
