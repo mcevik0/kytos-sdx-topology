00c75647a03ee5d1fe01a1bb82dfe47e41ed063e
lmarinve <lmarinve@gmail.com>
2023-07-11 20:32:08 +0000

    adding pytest

--- app/main.py
+++ app/main.py
@@ -2,11 +2,15 @@
 Main module of amlight/sdx Kytos Network Application.
 """
 import requests
+from napps.kytos.sdx_topology import settings  # pylint: disable=E0401
 from werkzeug.exceptions import BadRequest
 
-from kytos.core import KytosNApp, rest, log
+from kytos.core import KytosNApp, log, rest
 from kytos.core.helpers import listen_to
-from napps.kytos.sdx_topology import settings # pylint: disable=E0401
+
+
+HSH = "##########"
+
 
 class Main(KytosNApp):  # pylint: disable=R0904
     """Main class of amlight/sdx NApp.
@@ -22,10 +26,10 @@ class Main(KytosNApp):  # pylint: disable=R0904
 
         So, if you have any setup routine, insert it here.
         """
-        log.info("################################")
-        log.info("######### sdx topology #########")
-        log.info("################################")
-        self.event_info = {} # pylint: disable=W0201
+        log.info(f"{HSH}{HSH}{HSH}")
+        log.info(f"{HSH}sdx topology{HSH}")
+        log.info(f"{HSH}{HSH}{HSH}")
+        self.event_info = {}  # pylint: disable=W0201
 
     def execute(self):
         """Run after the setup method execution.
@@ -42,35 +46,44 @@ class Main(KytosNApp):  # pylint: disable=R0904
         If you have some cleanup procedure, insert it here.
         """
 
-
     @listen_to("kytos/topology.*")
-    def listen_topology(self, event=None):  # pylint: disable=no-self-use
-        """ Function meant for listen topology """
-        log.info("######### listen topology get kytos topology #########")
-        event_info = {} # pylint: disable=W0201
+    def listen_topology(self, event=None):
+        """Function meant for listen topology"""
+        f_name = " listen_topology "
+        log.info(f"{HSH}{f_name}get kytos topology {HSH}")
+        event_info = {}  # pylint: disable=W0201
         if event:
             allowed_events = [
-                    "kytos/topology.switch.enabled",
-                    "kytos/topology.switch.disabled",
-                    "kytos/topology.link_up",
-                    "kytos/topology.link_down"]
+                "kytos/topology.switch.enabled",
+                "kytos/topology.switch.disabled",
+                "kytos/topology.link_up",
+                "kytos/topology.link_down",
+            ]
             if event.name in allowed_events:
                 try:
                     topology = requests.get(settings.KYTOS_TOPOLOGY_URL).json()
-                    log.info("######### listen topology constructor post request #########")
-                    event_info = {"event": event, "kytostopology": topology} # pylint: disable=W0201
+                    log.info(
+                        f"{HSH}{f_name}constructor post request {HSH}"
+                    )
+                    event_info = {
+                        "event": event,
+                        "kytostopology": topology,
+                    }  # pylint: disable=W0201
                     try:
                         requests.post(settings.SDX_CONSTRUCTOR).json(event_info)
                     except Exception as err:  # pylint: disable=W0703
-                        log.info("######### constructor post error #########")
+                        log.info(f"{HSH}{f_name}constructor post error {HSH}")
                         log.info(err, event_info)
                 except Exception as err:  # pylint: disable=W0703
-                    log.info("######### listen topology get kytos topology  error #########")
+                    log.info(
+                            f"{HSH}{f_name}get kytos topology error {HSH}"
+                    )
                     log.info(err)
         return event_info
 
     @rest("v2/listen_topology", methods=["GET"])
     def get_listen_topology(self):
-        """ consume call listen Topology """
-        log.info("######### get_listen_topology #########")
+        """consume call listen Topology"""
+        f_name = " get_listen_topology "
+        log.info(f"{HSH}{f_name}{HSH}")
         return self.listen_topology()
--- app/settings.py
+++ app/settings.py
@@ -1,8 +1,7 @@
 """Module with the Constants used in the kytos/sdx_topology."""
 
 KYTOS_TOPOLOGY_URL = "http://localhost:8181/api/kytos/topology/v3/"
-GET_KYTOS_TOPOLOGY = \
-        "http://0.0.0.0:8181/api/kytos/sdx_topology/v1/listen_topology"
+GET_KYTOS_TOPOLOGY = "http://0.0.0.0:8181/api/kytos/sdx_topology/v1/listen_topology"
 SDX_CONSTRUCTOR = "http://0.0.0.0:8080/sdx/v2/constructor"
 SDX_PROVISIONING = "http://0.0.0.0:8088/sdx/v2/provision"
 HEADERS = {"Content-type": "application/json"}
