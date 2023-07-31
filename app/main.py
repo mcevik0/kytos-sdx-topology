3b48f46367efcb23a057f750e418a67cafdb7011
lmarinve <lmarinve@gmail.com>
2023-07-31 19:23:40 +0000

    validator

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 """
 Main module of amlight/sdx Kytos Network Application.
 """
+import os
+import shelve
 import requests
-from napps.kytos.sdx_topology import settings  # pylint: disable=E0401
+from flask import request, jsonify
 from werkzeug.exceptions import BadRequest
+from napps.kytos.sdx_topology.topology_class import (ParseTopology) \
+        # pylint: disable=E0401
+from napps.kytos.sdx_topology import settings, utils, topology_mock \
+        # pylint: disable=E0401
 
 from kytos.core import KytosNApp, log, rest
 from kytos.core.helpers import listen_to
 
-
 HSH = "##########"
+URN = "urn:sdx:"
 
 
 class Main(KytosNApp):  # pylint: disable=R0904
@@ -30,6 +36,7 @@ class Main(KytosNApp):  # pylint: disable=R0904
         log.info(f"{HSH}sdx topology{HSH}")
         log.info(f"{HSH}{HSH}{HSH}")
         self.event_info = {}  # pylint: disable=W0201
+        self.topology_loaded = False  # pylint: disable=W0201
 
     def execute(self):
         """Run after the setup method execution.
@@ -39,6 +46,7 @@ class Main(KytosNApp):  # pylint: disable=R0904
 
             self.execute_as_loop(30)  # 30-second interval.
         """
+        self.load_storehouse()
 
     def shutdown(self):
         """Run when your NApp is unloaded.
@@ -46,44 +54,126 @@ class Main(KytosNApp):  # pylint: disable=R0904
         If you have some cleanup procedure, insert it here.
         """
 
+    @listen_to("kytos/topology.unloaded")
+    def unload_topology(self):  # pylint: disable=W0613
+        """Function meant for validation, to make sure that the storehouse napp
+        has been loaded before all the other functions that use it begins to
+        call it."""
+        self.topology_loaded = False  # pylint: disable=W0201
+
+    def create_update_topology(self, event_type=0, event_timestamp=None):
+        """Function that will take care of initializing the namespace
+        kytos.storehouse.version within the storehouse and create a
+        box object containing the version data that will be updated
+        every time a change is detected in the topology."""
+        # open a shelf file
+        storehouse = shelve.open("storehouse")  # pylint: disable=W0201
+        if event_type == 1:
+            storehouse['version'] += 1
+            storehouse['timestamp'] = utils.get_timestamp()
+        elif event_type == 2:
+            storehouse['timestamp'] = event_timestamp
+        storehouse.sync()
+        self.storehouse = dict(storehouse)  # pylint: disable=W0201
+        return ParseTopology(
+            topology=self.get_kytos_topology(),
+            version=storehouse['version'],
+            timestamp=storehouse['timestamp'],
+            model_version=storehouse['model_version'],
+            oxp_name=storehouse['oxp_name'],
+            oxp_url=storehouse['oxp_url'],
+        ).get_sdx_topology()
+
+    def get_sdx_topology(self, event_type=0, event_timestamp=None):
+        """ return the topology following the SDX data model"""
+        if self.topology_loaded:
+            try:
+                if event_type != 0:
+                    topology_update = self.create_update_topology(
+                            event_type, event_timestamp)
+                    topology_dict = {
+                            "id": topology_update["id"],
+                            "name": topology_update["name"],
+                            "version": topology_update["version"],
+                            "model_version": topology_update["model_version"],
+                            "timestamp": topology_update["timestamp"],
+                            "nodes": topology_update["nodes"],
+                            "links": topology_update["links"],
+                            }
+                else:
+                    topology_dict = topology_mock.topology_mock()
+                validate_topology = requests.post(
+                        settings.SDX_TOPOLOGY_VALIDATE,
+                        timeout=10,
+                        json=topology_dict)
+                if validate_topology.status_code == 200:
+                    post_topology = requests.post(
+                            settings.SDX_LC_TOPOLOGY,
+                            timeout=10,
+                            json=topology_dict)
+                    if post_topology.status_code == 200:
+                        return (topology_dict, 200)
+                    return (post_topology.json(), 400)
+                return (validate_topology.json(), 400)
+            except Exception as err:  # pylint: disable=W0703
+                log.info(err)
+                return ("Validation Error", 400)
+        return ("No SDX Topology loaded", 401)
+
+    @rest("v1/topology")
+    def get_sdx_topology_loaded(self):
+        """ REST to return the sdx topology loaded"""
+        response, status_code = self.get_sdx_topology()
+        return jsonify(response), status_code
+
     @listen_to("kytos/topology.*")
     def listen_topology(self, event=None):
         """Function meant for listen topology"""
         f_name = " listen_topology "
         log.info(f"{HSH}{f_name}get kytos topology {HSH}")
-        event_info = {}  # pylint: disable=W0201
         if event:
             if event.name in settings.ADMIN_EVENTS:
-                pass
-            elif event.name in settings.ADMIN_EVENTS:
-                pass
+                event_type = 1
+            elif event.name in settings.OPERATIONAL_EVENTS and \
+                    event.timestamp is not None:
+                event_type = 2
             else:
-                return event_info
+                return {"event": "not action event"}
+            if self.storehouse:
+                if 'id' in self.storehouse.keys() and \
+                        'name' in self.storehouse.keys():
+                    self.topology_loaded = True  # pylint: disable=W0201
+                    return self.get_topology_version(
+                            event_type, event.timestamp)
+                return {"error": "not id or name in self.storehouse"}
+            return {"error": "not self.storehouse"}
+        return {"event": "None"}
 
-            try:
-                topology = requests.get(settings.KYTOS_TOPOLOGY_URL).json()
-                log.info(
-                    f"{HSH}{f_name}constructor post request {HSH}"
-                )
-                event_info = {
-                    "event": event,
-                    "kytostopology": topology,
-                }  # pylint: disable=W0201
-                try:
-                    requests.post(settings.SDX_CONSTRUCTOR).json(event_info)
-                except Exception as err:  # pylint: disable=W0703
-                    log.info(f"{HSH}{f_name}constructor post error {HSH}")
-                    log.info(err, event_info)
-            except Exception as err:  # pylint: disable=W0703
-                log.info(
-                        f"{HSH}{f_name}get kytos topology error {HSH}"
-                    )
-                log.info(err)
-        return event_info
-
-    @rest("v2/listen_topology", methods=["GET"])
+    @rest("v1/listen_topology", methods=["GET"])
     def get_listen_topology(self):
         """consume call listen Topology"""
         f_name = " get_listen_topology "
         log.info(f"{HSH}{f_name}{HSH}")
         return self.listen_topology()
+
+    def load_storehouse(self, event=None):  # pylint: disable=W0613
+        """Function meant for validation, to make sure that the storehouse
+        has been loaded before all the other functions that use it begins to
+        call it."""
+        if not self.topology_loaded:  # pylint: disable=E0203
+            # open a shelf file
+            storehouse = shelve.open("storehouse")  # pylint: disable=W0201
+            if 'id' not in dict(storehouse.keys()) or \
+                    'name' not in dict(storehouse.keys()):
+                # initialize sdx topology
+                storehouse['id'] = URN+"topology:"+os.environ.get("OXPO_URL")
+                storehouse['name'] = os.environ.get("OXPO_NAME")
+                storehouse['URL'] = os.environ.get("OXPO_URL")
+                storehouse['version'] = 0
+                storehouse['model_version'] = os.environ.get("MODEL_VERSION")
+                storehouse['timestamp'] = utils.get_timestamp()
+                storehouse['nodes'] = []
+                storehouse['links'] = []
+            self.storehouse = dict(storehouse)  # pylint: disable=W0201
+            # now, we simply close the shelf file.
+            storehouse.close()
--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,6 @@
 """SDX topology Utility functions"""
 from datetime import datetime
-from pathlib import Path
-from openapi_core import create_spec
-from openapi_core.contrib.flask import FlaskOpenAPIRequest
-from openapi_core.validation.request.validators import RequestValidator
-from openapi_spec_validator import validate_spec
-from openapi_spec_validator.readers import read_from_filename
-import numpy as np
-import pandas as pd
 import pytz
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
 
 
 def get_timestamp(timestamp=None):
@@ -22,70 +10,3 @@ def get_timestamp(timestamp=None):
             return timestamp[:10]+"T"+timestamp[11:19]+"Z"
     return datetime.now(
             pytz.timezone("America/New_York")).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-
-def diff_pd(current_params, initial_params):
-    """Identify differences between two pandas DataFrames"""
-    current_dict = [v for (k, v) in current_params.items()]
-    current_df = pd.json_normalize(current_dict)
-    initial_dict = [v for (k, v) in initial_params.items()]
-    initial_df = pd.json_normalize(initial_dict)
-
-    assert (current_df.columns == initial_df.columns).all(), \
-        "DataFrame column names are different"
-    if any(current_df.dtypes != initial_df.dtypes):
-        # Data Types are different, trying to convert
-        initial_df = initial_df.astype(current_df.dtypes)
-    if current_df.equals(initial_df):
-        return {'index': 'no changes', 'from': '', 'to': ''}
-    # need to account for np.nan != np.nan returning True
-    diff_mask = (current_df != initial_df) & \
-        ~(current_df.isnull() & initial_df.isnull())
-    ne_stacked = diff_mask.stack()
-    changed = ne_stacked[ne_stacked]
-    changed.index.names = ['id', 'col']
-    difference_locations = np.where(diff_mask)
-    changed_from = current_df.values[difference_locations]
-    changed_to = initial_df.values[difference_locations]
-    return {'index': changed.index[0][1],
-            'from': changed_from[0],
-            'to': changed_to[0]}
-
-
-def load_spec():
-    """Validate openapi spec."""
-    napp_dir = Path(__file__).parent
-    yml_file = napp_dir / "validator.yml"
-    spec_dict, _ = read_from_filename(yml_file)
-
-    validate_spec(spec_dict)
-
-    return create_spec(spec_dict)
-
-
-def validate_request(spec, data_request):
-    """Decorator to validate a REST endpoint input.
-
-    Uses the schema defined in the openapi.yml file
-    to validate.
-    """
-    validator = RequestValidator(spec)
-    openapi_request = FlaskOpenAPIRequest(data_request)
-    result = validator.validate(openapi_request)
-    error_response = {}
-    if result.errors:
-        errors = result.errors[0]
-        if hasattr(errors, "schema_errors"):
-            schema_errors = errors.schema_errors[0]
-            error_response = {
-                "error_message": schema_errors.message,
-                "error_validator": schema_errors.validator,
-                "error_validator_value": schema_errors.validator_value,
-                "error_path": list(schema_errors.path),
-                "error_schema": schema_errors.schema,
-                "error_schema_path": list(schema_errors.schema_path),
-                }
-        else:
-            error_response = {"errors": errors}
-        return (error_response, 400)
-    return (data_request.json, 200)
