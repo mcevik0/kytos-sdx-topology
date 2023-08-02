44a0e2ac489b9b91c1a1acb7c9b1f47da6deb8df
lmarinve <lmarinve@gmail.com>
2023-08-02 13:38:30 +0000

    adding get_kytos_topology

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ class Main(KytosNApp):  # pylint: disable=R0904
         log.info(f"{HSH}sdx topology{HSH}")
         log.info(f"{HSH}{HSH}{HSH}")
         self.event_info = {}  # pylint: disable=W0201
-        self.topology_loaded = False  # pylint: disable=W0201
+        self.shelve_loaded = False  # pylint: disable=W0201
 
     def execute(self):
         """Run after the setup method execution.
@@ -46,7 +46,7 @@ class Main(KytosNApp):  # pylint: disable=R0904
 
             self.execute_as_loop(30)  # 30-second interval.
         """
-        self.load_storehouse()
+        self.load_shelve()
 
     def shutdown(self):
         """Run when your NApp is unloaded.
@@ -56,82 +56,115 @@ class Main(KytosNApp):  # pylint: disable=R0904
 
     @listen_to("kytos/topology.unloaded")
     def unload_topology(self):  # pylint: disable=W0613
-        """Function meant for validation, to make sure that the storehouse napp
+        """Function meant for validation, to make sure that the shelve
         has been loaded before all the other functions that use it begins to
         call it."""
-        self.topology_loaded = False  # pylint: disable=W0201
+        self.shelve_loaded = False  # pylint: disable=W0201
+
+    def test_kytos_topology(self):
+        """ Test if the Topology napp has loaded """
+        try:
+            _ = self.get_kytos_topology()
+            return True
+        except Exception as err:  # pylint: disable=W0703
+            log.info(err)
+            return False
+
+    @staticmethod
+    def get_kytos_topology():
+        """retrieve topology from API"""
+        kytos_topology = requests.get(
+                settings.KYTOS_TOPOLOGY, timeout=10).json()
+        return kytos_topology["topology"]
 
     def create_update_topology(self, event_type=0, event_timestamp=None):
-        """Function that will take care of initializing the namespace
-        kytos.storehouse.version within the storehouse and create a
-        box object containing the version data that will be updated
+        """Function that will take care of initializing the topology shelve
+        and create a object containing the version data that will be updated
         every time a change is detected in the topology."""
-        # open a shelf file
-        storehouse = shelve.open("storehouse")  # pylint: disable=W0201
-        if event_type == 1:
-            storehouse['version'] += 1
-            storehouse['timestamp'] = utils.get_timestamp()
-        elif event_type == 2:
-            storehouse['timestamp'] = event_timestamp
-        storehouse.sync()
-        self.storehouse = dict(storehouse)  # pylint: disable=W0201
-        return ParseTopology(
-            topology=self.get_kytos_topology(),
-            version=storehouse['version'],
-            timestamp=storehouse['timestamp'],
-            model_version=storehouse['model_version'],
-            oxp_name=storehouse['oxp_name'],
-            oxp_url=storehouse['oxp_url'],
-        ).get_sdx_topology()
+        try:
+            # open the topology shelve
+            with shelve.open("topology_shelve") as open_shelve:
+                version = open_shelve['version']
+                self.dict_shelve = dict(open_shelve)
+                open_shelve.close()
+            if event_type == 1:
+                version += 1
+                timestamp = utils.get_timestamp()
+            elif event_type == 2:
+                timestamp = event_timestamp
+            return ParseTopology(
+                topology=self.get_kytos_topology(),
+                version=version,
+                timestamp=timestamp,
+                model_version=self.dict_shelve['model_version'],
+                oxp_name=self.dict_shelve['oxp_name'],
+                oxp_url=self.dict_shelve['oxp_url'],
+            ).get_sdx_topology()
+        except Exception as err:  # pylint: disable=W0703
+            log.info(err)
+            return ("Validation Error", 400)
 
     def get_sdx_topology(self, event_type=0, event_timestamp=None):
         """ return the topology following the SDX data model"""
-        if self.topology_loaded:
-            try:
-                if event_type != 0:
-                    topology_update = self.create_update_topology(
-                            event_type, event_timestamp)
-                    topology_dict = {
-                            "id": topology_update["id"],
-                            "name": topology_update["name"],
-                            "version": topology_update["version"],
-                            "model_version": topology_update["model_version"],
-                            "timestamp": topology_update["timestamp"],
-                            "nodes": topology_update["nodes"],
-                            "links": topology_update["links"],
-                            }
-                else:
-                    topology_dict = topology_mock.topology_mock()
-                validate_topology = requests.post(
-                        settings.SDX_TOPOLOGY_VALIDATE,
+        try:
+            if event_type != 0:
+                topology_update = self.create_update_topology(
+                        event_type, event_timestamp)
+                sdx_topology = {
+                        "id": topology_update["id"],
+                        "name": topology_update["name"],
+                        "version": topology_update["version"],
+                        "model_version": topology_update["model_version"],
+                        "timestamp": topology_update["timestamp"],
+                        "nodes": topology_update["nodes"],
+                        "links": topology_update["links"],
+                        }
+            else:
+                sdx_topology = topology_mock.topology_mock()
+            validate_topology = requests.post(
+                    settings.SDX_TOPOLOGY_VALIDATE,
+                    timeout=10,
+                    json=sdx_topology)
+            if validate_topology.status_code == 200:
+                post_topology = requests.post(
+                        settings.SDX_LC_TOPOLOGY,
                         timeout=10,
-                        json=topology_dict)
-                if validate_topology.status_code == 200:
-                    post_topology = requests.post(
-                            settings.SDX_LC_TOPOLOGY,
-                            timeout=10,
-                            json=topology_dict)
-                    if post_topology.status_code == 200:
-                        return (topology_dict, 200)
-                    return (post_topology.json(), 400)
-                return (validate_topology.json(), 400)
-            except Exception as err:  # pylint: disable=W0703
-                log.info(err)
-                return ("Validation Error", 400)
+                        json=sdx_topology)
+                if post_topology.status_code == 200:
+                    if event_type != 0:
+                        # open the topology shelve
+                        with shelve.open("topology_shelve") as open_shelve:
+                            open_shelve['version'] = sdx_topology["version"]
+                            open_shelve['timestamp'] = \
+                                sdx_topology["timestamp"]
+                            open_shelve['nodes'] = sdx_topology["nodes"]
+                            open_shelve['links'] = sdx_topology["links"]
+                            # now, we simply close the shelf file.
+                            open_shelve.close()
+                    return (sdx_topology, 200)
+                return (post_topology.json(), 400)
+            return (validate_topology.json(), 400)
+        except Exception as err:  # pylint: disable=W0703
+            log.info(err)
         return ("No SDX Topology loaded", 401)
 
     @rest("v1/topology")
-    def get_sdx_topology_loaded(self):
+    def get_sdx_topology_loaded(self, event_type=0, event_timestamp=None):
         """ REST to return the sdx topology loaded"""
-        response, status_code = self.get_sdx_topology()
-        return jsonify(response), status_code
+        return self.get_sdx_topology(event_type, event_timestamp)
 
     @listen_to("kytos/topology.*")
     def listen_topology(self, event=None):
         """Function meant for listen topology"""
         f_name = " listen_topology "
         log.info(f"{HSH}{f_name}get kytos topology {HSH}")
-        if event:
+        # open the event shelve
+        with shelve.open("events_shelve") as log_events:
+            shelve_events = log_events['events']
+            shelve_events.append(event)
+            log_events['events'] = shelve_events
+            log_events.close()
+        if event is not None and self.get_kytos_topology():
             if event.name in settings.ADMIN_EVENTS:
                 event_type = 1
             elif event.name in settings.OPERATIONAL_EVENTS and \
@@ -139,41 +172,58 @@ class Main(KytosNApp):  # pylint: disable=R0904
                 event_type = 2
             else:
                 return {"event": "not action event"}
-            if self.storehouse:
-                if 'id' in self.storehouse.keys() and \
-                        'name' in self.storehouse.keys():
-                    self.topology_loaded = True  # pylint: disable=W0201
-                    return self.get_topology_version(
-                            event_type, event.timestamp)
-                return {"error": "not id or name in self.storehouse"}
-            return {"error": "not self.storehouse"}
-        return {"event": "None"}
+            return self.get_sdx_topology(event_type, event.timestamp)
+        log.info(f"{HSH} event:{event}, topology: {self.get_kytos_topology()}")
+        return {"event": event, "topology": self.get_kytos_topology()}
 
     @rest("v1/listen_topology", methods=["GET"])
-    def get_listen_topology(self):
+    def get_listen_topology(self, event=None):
         """consume call listen Topology"""
         f_name = " get_listen_topology "
         log.info(f"{HSH}{f_name}{HSH}")
-        return self.listen_topology()
+        return self.listen_topology(event)
 
-    def load_storehouse(self, event=None):  # pylint: disable=W0613
-        """Function meant for validation, to make sure that the storehouse
+    def load_shelve(self):  # pylint: disable=W0613
+        """Function meant for validation, to make sure that the store_shelve
         has been loaded before all the other functions that use it begins to
         call it."""
-        if not self.topology_loaded:  # pylint: disable=E0203
-            # open a shelf file
-            storehouse = shelve.open("storehouse")  # pylint: disable=W0201
-            if 'id' not in dict(storehouse.keys()) or \
-                    'name' not in dict(storehouse.keys()):
-                # initialize sdx topology
-                storehouse['id'] = URN+"topology:"+os.environ.get("OXPO_URL")
-                storehouse['name'] = os.environ.get("OXPO_NAME")
-                storehouse['URL'] = os.environ.get("OXPO_URL")
-                storehouse['version'] = 0
-                storehouse['model_version'] = os.environ.get("MODEL_VERSION")
-                storehouse['timestamp'] = utils.get_timestamp()
-                storehouse['nodes'] = []
-                storehouse['links'] = []
-            self.storehouse = dict(storehouse)  # pylint: disable=W0201
-            # now, we simply close the shelf file.
-            storehouse.close()
+        if not self.shelve_loaded:  # pylint: disable=E0203
+            # open the sdx topology shelve file
+            with shelve.open("topology_shelve") as open_shelve:
+                if 'id' not in open_shelve.keys() or \
+                        'name' not in open_shelve.keys():
+                    # initialize sdx topology
+                    open_shelve['id'] = URN+"topology:"+os.environ.get(
+                            "OXPO_URL")
+                    open_shelve['name'] = os.environ.get("OXPO_NAME")
+                    open_shelve['url'] = os.environ.get("OXPO_URL")
+                    open_shelve['version'] = 0
+                    open_shelve['model_version'] = os.environ.get(
+                            "MODEL_VERSION")
+                    open_shelve['timestamp'] = utils.get_timestamp()
+                    open_shelve['nodes'] = []
+                    open_shelve['links'] = []
+                self.dict_shelve = dict(open_shelve)  # pylint: disable=W0201
+                self.shelve_loaded = True  # pylint: disable=W0201
+                # now, we simply close the shelf file.
+                open_shelve.close()
+            # open the events shelve file
+            with shelve.open("events_shelve") as events_shelve:
+                events_shelve['events'] = []
+                events_shelve.close()
+
+    @rest("v1/shelve/topology", methods=["GET"])
+    def get_shelve_topology(self):
+        """return sdx topology shelve"""
+        open_shelve = shelve.open("topology_shelve")
+        dict_shelve = dict(open_shelve)
+        open_shelve.close()
+        return dict_shelve
+
+    @rest("v1/shelve/events", methods=["GET"])
+    def get_shelve_events(self):
+        """return events shelve"""
+        open_shelve = shelve.open("events_shelve")
+        dict_shelve = dict(open_shelve)
+        open_shelve.close()
+        return dict_shelve
